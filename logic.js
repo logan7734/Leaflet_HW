ec94be0879b49870dca1cc6ff46b9c79327cbad8
logan7734 <logan7734@gmail.com>
2019-07-11 18:39:57 -0500

    .

--- logic.js
+++ logic.js
@@ -4,7 +4,7 @@ const myMap = L.map("map", {
   });
   
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>, <br> Created by Jonathan Randolph",
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>,,
     maxZoom: 18,
     id: "mapbox.dark",
     accessToken: API_KEY
